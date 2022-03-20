d5d5a9d8e399a1bf61da31a0e5dc0ae9e2570c22
Dipayan Singha Roy <dipayan95.singharoy@gmail.com>
2022-03-20 16:43:26 +0530

    fragment_add_hotel_check_in.xml fragment_add_journey.xml fragment_add_sightseeing.xml populated and constructed

--- app/src/main/java/com/iamdsr/travel/models/ItineraryModel.kt
+++ app/src/main/java/com/iamdsr/travel/models/ItineraryModel.kt
@@ -1,12 +1,11 @@
 package com.iamdsr.travel.models
 
-import java.sql.Timestamp
-
 data class ItineraryModel (
     val itinerary_id: String,
     val title: String,
     val description: String,
     val date: String,
+    val time: String,
     val day: Long,
     val type: String,
     val journey_mode: String,
@@ -14,18 +13,19 @@ data class ItineraryModel (
     val to: String,
     val hotel_name: String,
     val hotel_address: String,
-    val time: String,
     val timestamp: String,
     val trip_id: String,
     val trip_title: String,
-    val user_id: String){
-    constructor() : this("","","","",0,"","","",
-        "","","","","","","","")
+    val user_id: String,
+    val position: Int){
+    constructor() : this("","","","","",0,"","",
+        "","","","","","","","", 0)
 
     override fun toString(): String {
-        return "ItineraryModel(itinerary_id='$itinerary_id', title='$title', description='$description', date='$date', day=$day, " +
-                "type='$type', journey_mode='$journey_mode', from='$from', to='$to', hotel_name='$hotel_name', hotel_address='$hotel_address', " +
-                "time='$time', timestamp='$timestamp', trip_id='$trip_id', trip_title='$trip_title', user_id='$user_id')"
+        return "ItineraryModel(itinerary_id='$itinerary_id', title='$title', description='$description'" +
+                ", start_date='$date', start_time='$time', day=$day, type='$type', journey_mode='$journey_mode'" +
+                ", from='$from', to='$to', hotel_name='$hotel_name', hotel_address='$hotel_address', timestamp='$timestamp', trip_id='$trip_id'" +
+                ", trip_title='$trip_title', user_id='$user_id', position=$position)"
     }
 
 
--- app/src/main/java/com/iamdsr/travel/planTrip/itineraries/TimelineFragment.kt
+++ app/src/main/java/com/iamdsr/travel/planTrip/itineraries/TimelineFragment.kt
@@ -1,17 +1,54 @@
 package com.iamdsr.travel.planTrip.itineraries
 
+import android.app.AlertDialog
 import android.os.Bundle
 import androidx.fragment.app.Fragment
 import android.view.LayoutInflater
 import android.view.View
 import android.view.ViewGroup
+import android.widget.Toast
+import androidx.navigation.fragment.findNavController
+import com.google.android.material.floatingactionbutton.ExtendedFloatingActionButton
 import com.iamdsr.travel.R
+import java.time.Duration
 
 
 class TimelineFragment : Fragment() {
 
+    private lateinit var addNewItinerary: ExtendedFloatingActionButton
     override fun onCreateView(inflater: LayoutInflater, container: ViewGroup?, savedInstanceState: Bundle?): View? {
         // Inflate the layout for this fragment
         return inflater.inflate(R.layout.fragment_timeline, container, false)
+    }
+
+    override fun onViewCreated(view: View, savedInstanceState: Bundle?) {
+        super.onViewCreated(view, savedInstanceState)
+        setupWidgets()
+        addNewItinerary.setOnClickListener(View.OnClickListener {
+            setupDialog()
+        })
+    }
+
+    private fun setupDialog() {
+        val builder = AlertDialog.Builder(context)
+        builder.setTitle("Select Itinerary Type")
+
+        val animals = arrayOf("Travel to Destination", "Hotel Check-In", "Roam nearby/ Sightseeing ")
+        builder.setItems(animals) { _, which ->
+            when (which) {
+                0 -> { findNavController().navigate(R.id.action_myItinerariesFragment_to_journeyFragment) }
+                1 -> { findNavController().navigate(R.id.action_myItinerariesFragment_to_addHotelCheckInFragment) }
+                2 -> { findNavController().navigate(R.id.action_myItinerariesFragment_to_addSightseeingFragment) }
+            }
+        }
+        val dialog = builder.create()
+        dialog.show()
+    }
+
+    private fun setupWidgets() {
+
+        if (view != null){
+            addNewItinerary = view!!.findViewById(R.id.add_new_itinerary)
+        }
     }
 }
